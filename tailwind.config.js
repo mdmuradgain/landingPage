11063bbab64630aa9c94db676e57ad3994855bd1
Murad <murad@gmail.com>
2024-10-11 21:29:46 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#ee0181',
+        'anuj': '#01ee91'
+      },
+      fontFamily:{
+        'anuj-font': ['Poppins', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
